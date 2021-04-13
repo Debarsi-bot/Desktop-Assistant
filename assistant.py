3e04d1bb9df736844127bfccb3b2d15dedec9859
Debarsi Sadhukhan <debarsi2000@gmail.com>
2021-04-13 16:58:20 +0530

    basic browsing capabalities

--- assistant.py
+++ assistant.py
@@ -1,6 +1,10 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+
 
 engine = pyttsx3.init()
 engine.setProperty('voice',engine.getProperty('voices')[1].id)
@@ -20,16 +24,53 @@ def inputVoice():
         audio = r.listen(source)
     try:
         query=r.recognize_google(audio,language='en-US')
-        print(query)
     except Exception as e:
         print(f"Repeat please{e}")
         return ""
     return query
     
+def moduleWikipedia(query):
+    query=query.replace('wikipedia',"")
+    try:
+        speak(wikipedia.summary(query,sentences=2))
+    except Exception:
+        speak("Could Not Find The Result")
+
+def moduleBrowse(query):
+    query=query.replace('browse',"").strip()
+    if "www." not in query:
+        query="www."+query
+    if ".com" not in query:
+        query=query+".com"
+    webbrowser.open(query)
+
+def google(query):
+    query=query.replace('google',"").strip()
+    webbrowser.open(f"www.google.com/search?q={query}")
+
+
 if __name__ == '__main__':
     greet()
     while(1):
-        query=inputVoice().lower
+        query=inputVoice().lower()
+        if(query in ["quit","terminate","exit","bye"]):
+            exit()
+        print(query)
+        if 'wikipedia' in query:
+            moduleWikipedia(query)
+        elif 'browse' in query:
+            moduleBrowse(query)
+        elif 'google' in query:
+            google(query)
+        elif 'open' in query:
+            os.system(query.replace("open","").strip())
+
+            
+
+            
+
+        
+
     
     
 
